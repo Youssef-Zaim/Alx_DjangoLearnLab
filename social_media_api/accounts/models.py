31f296fe256d3556608e4f86c984f0e6e04c9849
Youssef Zaim <74555534+Youssef-Zaim@users.noreply.github.com>
2025-12-12 19:04:01 +0100

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,11 +1,18 @@
+# accounts/models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager 
+from django.contrib.auth.models import AbstractUser
 
-# User model extended from AbstractUser
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    following = models.ManyToManyField('self', blank=True, related_name='followed_by', symmetrical=False)
-    
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    # followers: users who follow this user
+    # related_name='following' => for a user `u`, u.following.all() هي قائمة من المستخدمين الذين u يتابعهم.
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True,
+    )
+
     def __str__(self):
         return self.username
