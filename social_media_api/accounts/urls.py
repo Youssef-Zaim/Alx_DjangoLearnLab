dab0ec07e61cf44ff3f961978cf73240a22f131a
Youssef Zaim <74555534+Youssef-Zaim@users.noreply.github.com>
2025-12-12 19:34:03 +0100

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,17 +1,8 @@
-# accounts/urls.py
 from django.urls import path
-from .views import RegisterView, LoginTokenView, ProfileView, follow_toggle
-from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, LoginView, ProfileView
 
 urlpatterns = [
-    # Registration & Login (JWT)
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginTokenView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Profile
-    path('profile/', ProfileView.as_view(), name='profile'),
-
-    # Follow / Unfollow (toggle)
-    path('follow/<str:username>/', follow_toggle, name='follow-toggle'),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
